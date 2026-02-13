4c89b7b79f6a4e4d80e74db73d2a5a8b55e2ee44
owlet914 <1321361443@qq.com>
2026-02-13 16:50:22 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -6,9 +6,11 @@ cleanup() {
 }
 
 
-trap cleanup EXIT SIGINT
-#sleep 5h 
-for i in {0,1,2,3,4,5,6,7}
+
+trap cleanup EXIT SIGINT SIGTERM
+
+GPU_NUM=8
+for i in $(seq 0 $((GPU_NUM-1)))
 do
     CUDA_VISIBLE_DEVICES=$((i)) python eval_vln_r2r_6.py --model_id $((i+1))  &
 done
